716dde6df75b07ccb1218ec5d66abfecbbafbf45
Manas502 <manasdeep.02@gmail.com>
2021-06-03 13:36:24 +0530

    add env variables

--- server/server.js
+++ server/server.js
@@ -1,18 +1,29 @@
 import express from "express";
-const app = express();
+import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
 // const path = require('path');
 // import path from "path";
-import userRouter from "./routes/users.js";
-app.get('/home', (req,res) => {
-   res.render('/home')
-})
+import userRouter from "./routes/user.js";
+const app = express();
+dotenv.config();
 
-app.get('/crypto', (req,res) => {
-    res.render('/crypto')
-})
+app.use(bodyParser.json({ limit: '30mb', extended: true }))
+app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
+// app.get('/home', (req,res) => {
+//    res.render('/home')
+// })
+
+// app.get('/crypto', (req,res) => {
+//     res.render('/crypto')
+// })
 
 app.use('/user', userRouter)
 
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+const PORT = process.env.PORT|| 5000;
+mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
+  .catch((error) => console.log(`${error} did not connect`));
+
+mongoose.set('useFindAndModify', false);
