de890e36f3467bbed45b3a0fdeae246985a0d20e
Manas502 <manasdeep.02@gmail.com>
2021-06-11 14:16:47 +0530

    add homepage and svg

--- client/src/App.js
+++ client/src/App.js
@@ -3,16 +3,18 @@ import NavbarTop from './components/NavbarTop'
 import Home from './components/Homepage/Home'
 import Crypto from './components/Cryptopage/Crypto'
 import Auth from './components/Auth/Auth'
-import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import {BrowserRouter as Router, Switch, Route, Redirect} from 'react-router-dom';
 function App() {
   return (
     <Router>
       <NavbarTop />
       <div className="App">
         <Switch>
+        {/* <Route path="/" exact component={Home} /> */}
         <Route path="/home" exact component={Home} />
         <Route path="/crypto" exact component={Crypto} />
         <Route path="/auth" exact component={Auth} />
+        {/* <Redirect to="/" /> */}
         </Switch>
       </div>
     </ Router>
--- client/src/components/NavbarTop.js
+++ client/src/components/NavbarTop.js
@@ -1,9 +1,11 @@
 import { useState,useEffect } from 'react';
 import { Link } from 'react-router-dom';
-import { useHistory,useLocation } from 'react-router-dom';
+import { useHistory,useLocation, NavLink } from 'react-router-dom';
 import { Provider, useDispatch } from 'react-redux';
 import {createStore, combineReducers} from 'redux';
 import authReducer from './reducers/auth';
+import { Nav } from 'react-bootstrap';
+import './Navbar.css';
 
 
 const NavWrapper = () => {
@@ -35,29 +37,29 @@ const NavbarTop = () => {
     <>
      <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
   <div className="container-fluid">
-    <a className="navbar-brand" href="/home">FINAN$E</a>
+    <NavLink className="navbar-brand" to="/home">FINAN$E</NavLink>
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
     </button>
     <div className="collapse navbar-collapse" id="navbarNavDropdown">
       <ul className="navbar-nav">
         <li className="nav-item">
-          <a className="nav-link active" aria-current="page" href="/home">Home</a>
+          <NavLink activeClassName= 'menu_active' className="ml-auto nav-link item_margin" aria-current="page" to="/home">Home</NavLink>
         </li>
         <li className="nav-item">
-          <a className="nav-link" href="/crypto">Crypto</a>
+          <NavLink activeClassName= 'menu_active' className="nav-link item_margin" to="/crypto">Crypto</NavLink>
         </li>
       </ul>
       <ul className="navbar-nav">
         {user ? (
           <>
            <li className="nav-item">
-            <a className="nav-link">Hi, {user.result.name} </a>
+            <NavLink className="nav-link item_margin" to="/home">Hi, {user.result.name} </NavLink>
            </li>
-           <button class="btn btn-sm btn-outline-secondary" type="button" onClick={logout}>Logout</button>
+           <button class="btn btn-sm btn-outline-secondary item_margin" type="button" onClick={logout}>Logout</button>
            </ >
         ) : (
-          <button onClick={() => history.push('/auth')} class="btn btn-sm btn-outline-success" type="button">Sign In</button>
+          <button onClick={() => history.push('/auth')} class="btn btn-sm btn-outline-success item_margin" type="button">Sign In</button>
         )}
        
       </ul>
